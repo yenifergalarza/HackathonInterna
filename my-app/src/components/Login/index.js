3f1a6f5b594fa52f02bc5033999a008f18f60ea2
Valeria Berrocal <valeriaberrocal@MBPdeValeria2.wifi>
2019-08-19 17:59:38 -0500

    navbar

--- my-app/src/components/Login/index.js
+++ my-app/src/components/Login/index.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import Iniciar from '../Inicar-sesion';
-import Registro from '../Registro';
+import Registro from '../Registro/index';
 import MenuOpts from '../Options'
 const Login = () => {
     const [type, setType] = useState('Iniciar Sesión')
     return(
         <div>
             <ul className="nav nav-tabs w-100" role="tablist">
-              <MenuOpts click={() => setType('Iniciar Sesión')} options="Desayuno" aClass="nav-link active" />
-              <MenuOpts click={() => setType('Registarse')} options="Almuerzo" aClass="nav-link" />
+              <MenuOpts click={() => setType('Iniciar Sesión')} options="Iniciar Sesión" aClass="nav-link active" />
+              <MenuOpts click={() => setType('Registrarse')} options="Registrarse" aClass="nav-link" />
             </ul>
             <div data-testid='opt' className="card-columns">
               {type === 'Iniciar Sesión' && (
