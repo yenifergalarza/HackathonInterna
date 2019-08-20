dda8155392e33787a6235f2f6677ae2965d863ea
Valeria Berrocal <valeriaberrocal@MBPdeValeria2.wifi>
2019-08-19 20:07:31 -0500

    styles nav tabs

--- my-app/src/components/Options.js
+++ my-app/src/components/Options.js
@@ -3,8 +3,8 @@ import React, {useState} from 'react';
 const MenuOptions = ({click, options, aClass}) => {
   const [line, setLine] = useState(true)
   return (
-    <li  className="nav-item" onClick={click} style={{borderBottom: !line ? "1px solid green" : "" }}>
-      <a data-testid={options} className={aClass} data-toggle="tab" href="#" role="tab" onClick={() => line ? setLine(false) : setLine(true)}>{options}</a>
+    <li  className="nav-item" onClick={click} style={{textDecoration: !line ? "underline" : "" }}>
+      <a data-testid={options} className={aClass} data-toggle="tab" href="#" role="tab" onChange={() => line ? setLine(false) : setLine(true)}>{options}</a>
     </li>
   )
 };
