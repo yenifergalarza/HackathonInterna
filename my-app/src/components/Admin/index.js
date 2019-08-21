cb7a8d8cf565087f7607ea015422a5f2ee597d43
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-08-20 23:47:33 -0500

    sube a firebbase

--- my-app/src/components/Admin/index.js
+++ my-app/src/components/Admin/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { useCollection } from 'react-firebase-hooks/firestore';
+import{ ordersData} from '../../controller/Firebase/authentication';
+
 
 const Admin = () => {
+    const [value, loading, error] = useCollection(
+        ordersData,
+        {
+          snapshotListenOptions: { includeMetadataChanges: true },
+        }
+      );
     return(
-        <div>
-            ADMIN
-        </div>
+        <>
+           <h2>ADMIN</h2> 
+            <div>
+      <p>
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Collection: Loading...</span>}
+        {value && (
+          <span>
+         
+            {value.docs.map(doc => (
+              <div key={doc.id}>
+                  <img src={doc.data().cart[0].image} alt="img"></img>
+               <h2>{doc.data().name}</h2>
+              </div>
+            ))}
+          </span>
+        )}
+      </p>
+    </div>
+        </>
     )
 }
 export default Admin
--- my-app/src/components/Productos/index.js
+++ my-app/src/components/Productos/index.js
@@ -7,10 +7,11 @@ import ProductBar from "../Productos/ProductBar"
 import Header from '../header'
 import Saldo from './saldo'
 import MenuOpts from '../Options'
+import{ ordersData} from '../../controller/Firebase/authentication'
 const Index = () => {
   const [tipo, setTipo] = useState("ofertas");
   const [products, setProducts] = useState([...ofertasArray]);
- 
+  const [client, setClient] = useState(users[0].name);
 const [billProducts,setBillProducts]=useState(0);
   //Aumentar contidad de productos de la lista
   const addToCart = id => {
@@ -58,6 +59,18 @@ const getTotal = prodcts => {
   return emptyArrayContent;
 };
 
+const sendOrders = (products, clientName) => {
+  let prods =[...products];
+  let newP = prods.filter( prod => prod.counter>0)
+  console.log("entre a firebase", newP);
+  ordersData.add({
+    name: clientName,
+    cart: newP,
+    time: new Date()
+  });
+  /* setClient("");
+  setProducts([]); */
+};
 
   return (
     <>
@@ -65,7 +78,7 @@ const getTotal = prodcts => {
        <Saldo object={users[0]}
        plata={billProducts}/>
        <button onClick={()=>{getTotal(products)}} >Saca tu plata</button>
-       <button onClick={()=>{getTotal(products)}} >Saca tu plata</button>
+       <button onClick={()=>{sendOrders(products,client)}} >C logra enviar </button>
         <div className="fill-available align-items-center d-flex flex-column ">
             <ul className="nav justify-content-center" role="tablist">
               <MenuOpts click={() => {setTipo("ofertas")}} options="Ofertas" aClass="nav-item nav-link active text-color"/>
